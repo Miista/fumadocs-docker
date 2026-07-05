e5c5021bd1338e7b11916eb25bd1b929a8743b95
Søren Guldmund <palmund@gmail.com>
2026-07-05 20:18:35 +0200

    feat: redirect / to /docs (skip pointless landing page)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,5 @@
-import Link from 'next/link';
-import { appName } from '@/lib/shared';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">{appName}</h1>
-      <p>
-        <Link href="/docs" className="font-medium underline">
-          Browse the docs
-        </Link>
-      </p>
-    </div>
-  );
+  redirect('/docs');
 }
